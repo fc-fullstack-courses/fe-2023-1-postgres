f32db6bf61b2c40afec5a204ef4a8e84b3e9c4c5
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-04 20:36:41 +0300

    Add advanced insertion example

--- sql/dml.sql
+++ sql/dml.sql
@@ -1,2 +1,8 @@
 -- занесення даних у таблицю
-INSERT INTO users VALUES ('User', 'Userenko', 'mail@test.test', 1.68, true, '1993-02-07');
+INSERT INTO users 
+-- довільний порядок вставки даних
+(height, first_name, last_name, is_male, birthday, email) 
+VALUES 
+-- вставка декількох користувачів одночасно
+(2.1, 'Jane', 'Doe', false, '1973-11-13', 'jane@outlook.com'),;
+(1.72, 'Anna', 'Nesterenko', false, '1987-07-04', 'nesternko@outlook.com');
