82cdab07a3d36192bd778a1b05e15927a05ab341
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-04 19:11:07 +0300

    Add sql sublanguages cheatsheet

---
+++ sql/sublanguages.sql
@@ -0,0 +1,8 @@
+/*
+  SQL складається з 5 псевдомов
+    DDL (data defenition language) - робота зі структурою БД
+    DQL (data query language) - отримання даних
+    DML (data manipulation language) - маніпуляція з інформацією БД (вставка, зміна видалення даних)
+    DCL (data control language) - адміністрування БД
+    TCL (transaction control language) - транзакції
+*/
