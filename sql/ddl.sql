5690106557d7962fe3f6b2ca5f8c65790ccad8f6
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-05 19:52:45 +0300

    Add constraint renaming examples

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -4,10 +4,8 @@ CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(64) NOT NULL CHECK (first_name != ''),
   last_name VARCHAR(64) NOT NULL CHECK (last_name != ''),
-  email VARCHAR(256) NOT NULL UNIQUE CHECK (
-    email ~ '^[a-zA-Z0-9]{1,32}@[a-z]{2,32}\.[a-z]{2,32}$' -- регулярка
-  ),
-  height NUMERIC(3, 2) NOT NULL CHECK(
+  email VARCHAR(256) NOT NULL UNIQUE, 
+  height NUMERIC(3, 2) NOT NULL CONSTRAINT "invalid height" CHECK(
     height > 0.4
     AND height < 3
   ),
@@ -15,7 +13,10 @@ CREATE TABLE users(
   birthday DATE NOT NULL CHECK(
     birthday > '1900-01-01'
     AND birthday < current_date
-  ) -- UNIQUE(email) -- обмеження таблиці
+  ), -- UNIQUE(email) -- обмеження таблиці
+  CONSTRAINT "invalid email" CHECK (
+    email ~ '^[a-zA-Z0-9]{1,32}@[a-z]{2,32}\.[a-z]{2,32}$' -- регулярка
+  ),
 );
 /*
  символьні типи даних 
--- sql/dml.sql
+++ sql/dml.sql
@@ -7,4 +7,4 @@ VALUES
 (0.51, 'User', 'Userenko', '1999-01-01', 'asds@sadsa.asds', true),
 (1.62, 'User', 'Userenko', '2023-07-04', 'mail4@gmail.com', true),
 (1.35, 'User', 'Userenko', '1983-05-14', 'mail2@mail.com', NULL),
-(2.95, 'User', 'Userenko', '1999-01-01', 'mail3@mail.com', true);
+(3.95, 'User', 'Userenko', '1999-01-01', 'mail3@mail.com', true);
