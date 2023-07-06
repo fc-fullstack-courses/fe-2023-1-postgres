fa0a0b68451c9948aee1015a39e38680af681df6
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-06 18:00:04 +0300

    Add task solution

--- sql/dml.sql
+++ sql/dml.sql
@@ -4,7 +4,7 @@ INSERT INTO users
 ( height, first_name, last_name, birthday, email, is_male) 
 VALUES 
 -- вставка декількох користувачів одночасно
-(0.51, 'User', 'Userenko', '1999-01-01', 'asds@sadsa.asds', true),
-(1.62, 'User', 'Userenko', '2023-07-04', 'mail4@gmail.com', true),
-(1.35, 'User', 'Userenko', '1983-05-14', 'mail2@mail.com', NULL),
-(3.95, 'User', 'Userenko', '1999-01-01', 'mail3@mail.com', true);
+(0.51, 'User1', 'Userenko', '1999-01-01', 'asds@sadsa.asds', true),
+(1.62, 'User2', 'Userenko', '2023-07-04', 'mail4@gmail.com', true),
+(1.35, 'User3', 'Userenko', '1983-05-14', 'mail2@mail.com', NULL),
+(2.95, 'User4', 'Userenko', '1999-01-01', 'mail3@mail.com', true);
--- sql/tasks/users.sql
+++ sql/tasks/users.sql
@@ -10,5 +10,28 @@
       - вага користувача має бути у межах діапазону від 0 до 500
       - Новим обмеженням дати людиночитаємі назви
       - рейтинг має коливатися від 1 до 5
-
 */
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(64) NOT NULL,
+  last_name VARCHAR(64) NOT NULL,
+  email VARCHAR(256) NOT NULL UNIQUE, 
+  height NUMERIC(3, 2) NOT NULL CONSTRAINT "invalid height" CHECK(
+    height > 0.4
+    AND height < 3
+  ),
+  is_male BOOLEAN DEFAULT true, 
+  birthday DATE NOT NULL CHECK(
+    birthday > '1900-01-01'
+    AND birthday < current_date
+  ), 
+  "weight" NUMERIC(5,2) CONSTRAINT "weight must be in range 0 - 500" CHECK(weight > 0 AND weight < 500),
+  rating NUMERIC(2, 1) CONSTRAINT "rating must be in range 1 - 5" CHECK(rating >= 1 AND rating <= 5),
+  CONSTRAINT "Unique name pairs" UNIQUE (first_name, last_name),
+  CONSTRAINT "User must have correct name" 
+  CHECK(first_name != '' AND last_name != ''),
+  CONSTRAINT "invalid email" CHECK (
+    email ~ '^[a-zA-Z0-9]{1,32}@[a-z]{2,32}\.[a-z]{2,32}$' 
+  )
+);
