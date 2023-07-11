7442fb983b566315e0ab1cae009255ff35640d17
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-11 19:03:46 +0300

    Add aggregation tasks with solutions

--- sql/aggregation.sql
+++ sql/aggregation.sql
@@ -28,3 +28,25 @@ GROUP BY id;
 
 -- SELECT foot_size FROM users
 -- GROUP BY foot_size;
+/*
+Знайдіть
+  - середній зріст користувачів
+  - середній зріст чоловіків та жінок
+  - мінімальний розмір ноги чоловіків та жінок
+  - мінімальну, максимальну і серердню вагу чоловіків та жінок
+
+*/
+
+--   - середній зріст користувачів
+SELECT avg(height) FROM users;
+-- - середній зріст чоловіків та жінок
+SELECT avg(height), is_male FROM users
+GROUP BY is_male;
+-- - мінімальний розмір ноги чоловіків та жінок
+SELECT min(foot_size), is_male
+FROM users
+GROUP BY is_male;
+-- - мінімальну, максимальну і серердню вагу чоловіків та жінок
+SELECT is_male, min(weight), avg(weight), max(weight)
+FROM users
+GROUP BY is_male;
