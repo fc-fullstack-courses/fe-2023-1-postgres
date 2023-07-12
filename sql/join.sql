c907a02648243166db53cacc0f33ba7672da1322
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-12 19:52:45 +0300

    Add some examples

--- sql/join.sql
+++ sql/join.sql
@@ -13,4 +13,28 @@ JOIN orders ON users.id = customer_id;
 SELECT * FROM orders
 JOIN users ON customer_id = users.id
 JOIN manufacturers ON user_id = users.id;
---
+-- всі дані першого замовлення
+SELECT *
+FROM orders AS o
+JOIN products_to_orders pto ON o.id = pto.order_id
+JOIN products p ON p.id = pto.product_id
+WHERE o.id = 1;
+-- Отримати всі замовлення з їжею
+SELECT order_id
+FROM products p
+JOIN products_to_orders pto ON p.id = product_id
+WHERE p.category ILIKE 'food'
+GROUP BY order_id
+ORDER BY order_id ASC;
+-- Кількість товарів з категорії їжи в певному замовленні (34)
+SELECT count(*)
+FROM products_to_orders pto
+JOIN products p ON p.id = pto.product_id
+WHERE p.category ILIKE 'food' AND order_id = 288;
+-- Кількість товарів з категорії їжи в кожному замовленні
+SELECT count(*), order_id
+FROM products_to_orders pto
+JOIN products p ON p.id = pto.product_id
+WHERE p.category ILIKE 'food'
+GROUP BY order_id
+ORDER BY count(*) ASC;
