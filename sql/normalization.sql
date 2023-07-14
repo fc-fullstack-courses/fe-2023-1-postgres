0db6e6f3c2c9c76fc9d08a93faaa24d37327a026
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-14 19:41:07 +0300

    Add 2NF example

--- sql/normalization.sql
+++ sql/normalization.sql
@@ -14,9 +14,28 @@ CREATE TABLE users(
   first_name TEXT,
   last_name TEXT
 ); 
--- 1 НФ
+-- 1 НФ (і 2 НФ)
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   first_name TEXT,
   last_name TEXT
 );
+-- не 2 НФ
+CREATE TABLE teachers_to_courses (
+  teacher_id INT,
+  course_name TEXT,
+  teachers_birthday DATE,
+  PRIMARY KEY (teacher_id, course_name)
+);
+-- 2 НФ
+CREATE TABLE teachers_to_courses (
+  teacher_id INT,
+  course_name TEXT,
+  PRIMARY KEY (teacher_id, course_name)
+);
+--
+CREATE TABLE teachers_data (
+  id SERIAL PRIMARY KEY,
+  teachers_birthday DATE
+);
+--
