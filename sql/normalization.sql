68762c2a1453cbce1e1c9b4321e9397346574317
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-14 19:54:55 +0300

    Add 3NF

--- sql/normalization.sql
+++ sql/normalization.sql
@@ -38,4 +38,42 @@ CREATE TABLE teachers_data (
   id SERIAL PRIMARY KEY,
   teachers_birthday DATE
 );
+/*
+транзитивна функціональна залежність - залежність не тількі від ключа а і від якогось неключового атрибута
+добре:
+id > attr1
+id > attr2
+
+погано:
+id > attr1 > attr2
+*/
+-- не 3 НФ
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  email TEXT,
+  department TEXT,
+  department_phone_number VARCHAR(64)
+);
+--
+INSERT INTO employees
+(name, email, department, department_phone_number)
+VALUES 
+('Test', 'test@test.test', 'HR', '147852369'),
+('Test2', 'test2@test.test', 'IT', '963852741'),
+('Test3', 'test3@test.test', 'QA', '987654321'),
+('Test4', 'test4@test.test', 'HR', '987654321');
+-- 3 НФ
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  email TEXT,
+  department_id INT
+);
+--
+CREATE TABLE departments(
+  id SERIAL PRIMARY KEY,
+  department TEXT,
+  department_phone_number VARCHAR(64)
+);
 --
